12440854a3c5bf378a7263390d3d4cdecd3a1032
Tims Guynes <tims.m.guynes@gmail.com>
2023-02-26 22:34:27 -0800

    first commit

---
+++ TestProject/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+*/
+
+//int result = Random.Next();
+/*
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+*/
+/*
+ Random dice = new Random();
+ int roll1 = dice.Next();
+ int roll2 = dice.Next(101);
+ int roll3 = dice.Next(50, 101);
+
+ Console.WriteLine($"First roll: {roll1}");
+ Console.WriteLine($"Second roll: {roll2}");
+ Console.WriteLine($"Third roll: {roll3}");
+
+ dice.Next()
+ */
+
+ int firstValue = 500;
+ int secondValue = 600;
+ int largerValue = Math.Max(firstValue, secondValue);
+
+ 
+
+ Console.WriteLine(largerValue);
