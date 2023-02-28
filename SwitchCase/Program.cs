e6ebc30b46ec46e38212003b3091ba8fef6ade33
Tims Guynes <tims.m.guynes@gmail.com>
2023-02-27 22:15:14 -0800

    update switch case challenge

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+*/
+
+//Switch-Case Challenge https://learn.microsoft.com/en-us/training/modules/csharp-switch-case/3-challenge
+
