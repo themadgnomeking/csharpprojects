305c521b411908f6685cbe2f3dd945d9dc7b0912
Tims Guynes <tims.m.guynes@gmail.com>
2023-02-27 21:24:53 -0800

    update variable scope challenge

--- ControlVariableScope/Program.cs
+++ ControlVariableScope/Program.cs
@@ -15,3 +15,21 @@ if (flag) Console.WriteLine(flag);
 */
 
 //Variable Scope challenge https://learn.microsoft.com/en-us/training/modules/csharp-code-blocks/4-exercise-challenge-variable-scope
+
+using System;
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;  
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine("Total: " + total);
