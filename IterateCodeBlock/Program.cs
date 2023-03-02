12aa7e7d34032cb6d851a016b1eaad6bcf626b18
Tims Guynes <tims.m.guynes@gmail.com>
2023-03-02 14:38:48 -0800

    fizzbuzz challenge

--- IterateCodeBlock/Program.cs
+++ IterateCodeBlock/Program.cs
@@ -1,5 +1,85 @@
-﻿
+﻿/*
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine(i);
+}
+*/
+/*
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+*/
+/*
+for (int i = 0; i < 10; i+=3)
+{
+    Console.WriteLine(i);
+}
+*/
+/*
+for (int i=0; i<10; i++)
+{
+    Console.WriteLine(i);
+    if (i==7) break;
+}
+*/
+
+//string[] names = { "Alex", "Eddie", "David", "Michael" };
+/*
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+*/
+/*
+foreach (var name in names)
+{
+    // Cant do this:
+    if (name == "David") name = "Sammy";
+}
+*/
+/*
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+*/
+/*
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David") names[i] = "Sammy";
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+*/
+
+//***********FIZZBUZZ
+string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 0; i <= 100; i++)
+{
+    if (i % 5 == 0)
+    {   
+        if (i % 3 == 0)
+        {
+            Console.WriteLine(fizz + buzz);
+        }
+        else
+        {
+        Console.WriteLine(buzz);
+        }
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine(fizz);
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+    
 }
