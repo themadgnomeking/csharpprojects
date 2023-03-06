83addbba778212eaa53349e1bde2b1cd2a50d055
Tims Guynes <tims.m.guynes@gmail.com>
2023-03-05 16:17:12 -0800

    completed challenge 3

--- ConvertDataCasting/Program.cs
+++ ConvertDataCasting/Program.cs
@@ -72,7 +72,7 @@ Console.WriteLine($"Measurement (w/ offset): {50 + result}");
 
 //Challenge - combine string arravy values https://learn.microsoft.com/en-us/training/modules/csharp-convert-cast/4-challenge
 
-
+/*
 
 // Rule 1: if the value is alphanumeric, concatinate it to form a message
 // Rule 2: if the value is numeric, add it to the total
@@ -102,3 +102,21 @@ foreach (var value in values)
 
 Console.WriteLine($"Message: {message}");
 Console.WriteLine($"Total: {total}");
+*/
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+int result1 = (int)value1 / (int)value2;
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = (decimal)value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = (float)value3 / (float)value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
