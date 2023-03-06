d50b811bcda07b32fb65f3d3afc8601aea0fd36c
Tims Guynes <tims.m.guynes@gmail.com>
2023-03-05 16:11:10 -0800

    completed convert cast challenge 2

--- ConvertDataCasting/Program.cs
+++ ConvertDataCasting/Program.cs
@@ -51,3 +51,54 @@ Console.WriteLine(value);
 int value2 = Convert.ToInt32(1.5m); // converting rounds up
 Console.WriteLine(value2);
 */
+/*
+string name = "Bob";
+Console.WriteLine(int.Parse(name));
+*/
+/*
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement.");
+}
+
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+*/
+
+//Challenge - combine string arravy values https://learn.microsoft.com/en-us/training/modules/csharp-convert-cast/4-challenge
+
+
+
+// Rule 1: if the value is alphanumeric, concatinate it to form a message
+// Rule 2: if the value is numeric, add it to the total
+// Rule 3: The result should match the following output
+// Message: ABCDEF
+// Total: 68.3
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+string message = "";
+decimal total = 0m;
+
+foreach (var value in values)
+{
+    decimal number; 
+    if (decimal.TryParse(value, out number))
+    {
+        
+        total += number;
+    }
+    else
+    {
+        message += value;
+        //Console.WriteLine(value);
+    }
+    
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
