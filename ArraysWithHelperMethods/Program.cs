458b1f316ff28ebe5889b211ffb5ddd7a31f74a3
Tims Guynes <tims.m.guynes@gmail.com>
2023-03-06 13:26:42 -0800

    updated array operations

--- ArraysWithHelperMethods/Program.cs
+++ ArraysWithHelperMethods/Program.cs
@@ -1,6 +1,6 @@
-﻿
+﻿/*
 string[] pallets = { "B14", "A11", "B12", "A13" };
-/*
+
 Console.WriteLine("Sorted...");
 Array.Sort(pallets);
 foreach (var pallet in pallets)
@@ -15,7 +15,7 @@ foreach (var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
 }
-*/
+
 Console.WriteLine("");
 
 
@@ -45,4 +45,19 @@ Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 foreach( var pallet in pallets)
 {
     Console.WriteLine($"-- {pallet}");
+}
+*/
+
+// Excersize - Discover Split() and Join()
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+//string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string [] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
 }
